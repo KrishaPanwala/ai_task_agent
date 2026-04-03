04eb07fbb625df254544113f498103d8a6912ba4
KrishaPanwala <panwalakrisha3@gmail.com>
2026-04-03 11:37:09 +0530

    version1

--- app/main.py
+++ app/main.py
@@ -57,8 +57,9 @@ app.mount(
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html",
+        context={}
     )
 
 
