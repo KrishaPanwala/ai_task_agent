b5a2819b5d9fcdebec8a27015a89539077243d54
KrishaPanwala <panwalakrisha3@gmail.com>
2026-04-04 20:32:31 +0530

    message issue6

--- app/db.py
+++ app/db.py
@@ -1,17 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.config import DATABASE_URL
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./tasks.db"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
